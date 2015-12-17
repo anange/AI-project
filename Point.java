4918f1bd5d7886d92ab4af8fa478dfd7055b11af
anange <a.angelakis@outlook.com>
2015-12-18 01:20:31 +0200

    Added Point class

---
+++ Point.java
@@ -0,0 +1,16 @@
+public class Point {
+
+    public int x;
+    public int y;
+
+    public Point() {
+        this.x = -1;
+        this.y = -1;
+    }
+
+    public Point(int i, int j) {
+        this.x = i;
+        this.y = j;
+    }
+
+}
