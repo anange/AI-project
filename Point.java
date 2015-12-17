c63f244c53123477bf82160fc9a3b71a57ba0540
anange <a.angelakis@outlook.com>
2015-12-18 01:47:08 +0200

    Added step attribute to Point for Path building

--- Point.java
+++ Point.java
@@ -2,15 +2,25 @@ public class Point {
 
     public int x;
     public int y;
+    private int step;
 
     public Point() {
         this.x = -1;
         this.y = -1;
+        this.step = -1;
     }
 
     public Point(int i, int j) {
         this.x = i;
         this.y = j;
+        this.step = -1;
+    }
+    
+    public int getStep() {
+        return this.step;
     }
 
+    public int setStep(int s) {
+        this.step = s;
+    }
 }
