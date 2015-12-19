2a96f0ed0cc407c03248e1c5615ff31759b2aa6a
anange <a.angelakis@outlook.com>
2015-12-19 18:39:29 +0200

    Added methods and constructors

--- State.java
+++ State.java
@@ -2,15 +2,33 @@ import java.util.ArrayList;
 
 public class State {
 
-    private int Square;
+    private Point Square;
     private State parent;
     private int step;
     private int h;
-    private int g;
+    private int g; //Always same as step :(
     
     //Is it really needed?
     private ArrayList<State> children;  
+    
+    // Point passes a reference - be careful with that
+    public State(State par, Point sq, Point goal) {
+        this.parent = par;
+        this.Square = sq;
+        this.g = this.parent.getStep() + 1;
+        this.step = this.g;
+        this.h = dist(this.Square, goal);
+    }
 
+    public State(Point sq, Point goal) {
+        this.parent = null;
+        this.Square = sq;
+        this.g = 0;
+        this.step = this.g;
+        this.h = dist(this.Square, goal);
+    }
+    
+    
     public int getH() {
         return this.h;
     }
@@ -18,4 +36,22 @@ public class State {
     public int getG() {
         return this.g;
     }
+
+    public int getStep() {
+        return this.step;
+    }
+
+    public Point getSq() {
+        return this.Square;
+    }
+
+    public State getParent() {
+        return this.parent;
+    }
+    
+    private static int dist(Point a, Point b) {
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
+
+
 }    
